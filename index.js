f3796e5a00b5c3d1018cd97dda54dabf8026240e
spykins <waleola.akinsanmi@gmail.com>
2018-08-24 12:07:12 -0400

    Allows you to read the key for a symbol

--- index.js
+++ index.js
@@ -8,30 +8,35 @@
  */
 
 
-const MY_FIRST_NAME = "Waleola";
+const FIRST_NAME = "Wale";
 // I want to create a Symbol
-const myFirstNameSymbolWithoutReference = Symbol(MY_FIRST_NAME);
-console.log("1: ", typeof myFirstNameSymbolWithoutReference);  // o/p  1:  symbol 
+const firstNameSymbolWithoutReference = Symbol(FIRST_NAME);
+console.log("1: ", typeof firstNameSymbolWithoutReference);  // o/p  1:  symbol 
 //This returns a new type Symbol
 
-const myFirstNameSymbolWithoutReferenceWithSameName = Symbol(MY_FIRST_NAME);
+const firstNameSymbolWithoutReferenceWithSameName = Symbol(FIRST_NAME);
 // I have the same name, will myFirstNameSymbolWithoutReference be equal to myFirstNameSymbolWithoutReferenceWithSameName?
-console.log("2: ", myFirstNameSymbolWithoutReference === myFirstNameSymbolWithoutReferenceWithSameName); // o/p 2:  false
+console.log("2: ", firstNameSymbolWithoutReference === firstNameSymbolWithoutReferenceWithSameName); // o/p 2:  false
 // This is because Symbol generates a unique identifier that you don't have access to
 // How do we create a Symbol that we can reference without knowing the unique identifier that is abstracted away from us
 
 // Use the Symbol.for("keyYouWantTOUSE")
 
-const myFirstNameSymbolThatICanReference = Symbol.for(MY_FIRST_NAME);
-console.log("3: ", typeof myFirstNameSymbolThatICanReference); //o/p 3:  symbol
+const firstNameSymbolThatICanReference = Symbol.for(FIRST_NAME);
+console.log("3: ", typeof firstNameSymbolThatICanReference); //o/p 3:  symbol
 // This still gives the output type to be a symbol...
 // Why is this useful?  Ok, we will create another variable and then see if both is equal
-const myFirstNameSymbolThatICanReferenceToTest = Symbol.for(MY_FIRST_NAME);
+let firstNameSymbolThatICanReferenceToTest = Symbol.for(FIRST_NAME);
 
 // checking if the two variables are equal
-console.log("4: ", myFirstNameSymbolThatICanReference === myFirstNameSymbolThatICanReferenceToTest); // o/p 4:  true
+console.log("4: ", firstNameSymbolThatICanReference === firstNameSymbolThatICanReferenceToTest); // o/p 4:  true
 
 // I think the best way to use a symbol is to use the Symbol.for 
+
+// We can also get the key used to create Symbols that we created with Symbol.for("key").. We can't get the name in Symbol();
+
+console.log("5: ",Symbol.keyFor(firstNameSymbolWithoutReference));  // o/p 5:  undefined
+console.log("6: ",Symbol.keyFor(firstNameSymbolThatICanReference)); // o/p 6:  Wale
 
 /**
  * This coding series will cover how to use private instance variable in javascript Object to attain real abstraction
