d79dba03331c3967da034dee7869723419b9e4af
spykins <waleola.akinsanmi@gmail.com>
2018-08-24 11:32:35 -0400

    Explains the two ways to create symbol in javascript

---
+++ index.js
@@ -0,0 +1,30 @@
+const MY_FIRST_NAME = "Waleola";
+// I want to create a Symbol
+const myFirstNameSymbolWithoutReference = Symbol(MY_FIRST_NAME);
+console.log("1: ", typeof myFirstNameSymbolWithoutReference);  // o/p  1:  symbol 
+//This returns a new type Symbol
+
+const myFirstNameSymbolWithoutReferenceWithSameName = Symbol(MY_FIRST_NAME);
+// I have the same name, will myFirstNameSymbolWithoutReference be equal to myFirstNameSymbolWithoutReferenceWithSameName?
+console.log("2: ", myFirstNameSymbolWithoutReference === myFirstNameSymbolWithoutReferenceWithSameName); // o/p 2:  false
+// This is because Symbol generates a unique identifier that you don't have access to
+// How do we create a Symbol that we can reference without knowing the unique identifier that is abstracted away from us
+
+// Use the Symbol.for("keyYouWantTOUSE")
+
+const myFirstNameSymbolThatICanReference = Symbol.for(MY_FIRST_NAME);
+console.log("3: ", typeof myFirstNameSymbolThatICanReference); //o/p 3:  symbol
+// This still gives the output type to be a symbol...
+// Why is this useful?  Ok, we will create another variable and then see if both is equal
+const myFirstNameSymbolThatICanReferenceToTest = Symbol.for(MY_FIRST_NAME);
+
+// checking if the two variables are equal
+console.log("4: ", myFirstNameSymbolThatICanReference === myFirstNameSymbolThatICanReferenceToTest); // o/p 4:  true
+
+// I think the best way to use a symbol is to use the Symbol.for 
+
+/**
+ * This coding series will cover how to use private instance variable in javascript Object to attain real abstraction
+ * How to use Iterators
+ * How to use generators in javascript
+ */
