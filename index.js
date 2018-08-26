446e1c4037ae149fbeff7c8d07e94af62862ca3b
spykins <waleola.akinsanmi@gmail.com>
2018-08-26 09:38:05 -0400

    write simple iterator code using the food I like

--- index.js
+++ index.js
@@ -89,3 +89,58 @@ console.log("6: ",Symbol.keyFor(firstNameSymbolThatICanReference)); // o/p 6:  W
 
 
  */
+
+ // Symbol.iterator
+ // This is a classic of what javascript has built in es6, I think its pretty cool and you can dive into their documentation here
+ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol/iterator
+ // I will try to simplify it as simple as I can
+
+ let foodILike = ["Pounded Yam", "eba", "fufu", "parota", "pan cake", "egunsi soup", "cabonara", "rice! rice!! rice!!"];
+ // Ok that explains why I am big and strong, I can't bet if they are healthy stuff though
+ // ok say we want to loop through what I have... we can do it in so many ways...
+
+ //****** looping through arraay, irrelevant on
+ /**
+  for (let index =0; index < foodILike.length; index++) {
+   console.log(foodILike[index])
+ }
+
+ // Used in object, since array is an object, I can use foodIlike[key]
+ for (let key in foodILike) {
+   console.log(foodILike[key]);
+ }
+
+ // The most prefer style for me is to use the functional paradigm of foreach
+
+ foodILike.forEach((value) => console.log(value));
+
+ // bomb! I should like that coming from Java background..... 
+
+  */
+  // Totally irrelevant so I will comment them out, you probably no over hundred ways to do that
+
+  //******** back to Iterator
+
+
+let iteratorOfFoodILike = foodILike[Symbol.iterator];
+console.log("7: ", typeof iteratorOfFoodILike); // o/p 7:  function
+// This is unsurprisingly a function
+// what do you do to a function?..... You execute it right?? with what??? just appending the () after the name
+
+// iteratorOfFoodILike();
+// for some reasons, I find out that executing the function this way throws an error, I will be glad if you can
+// take a look at it and tell me why it is failing.....
+// but I can execute right here
+iteratorOfFoodILike = foodILike[Symbol.iterator]();
+// Just reassigning the variable... = assignment operator means whatever is on the right to the left.... TIL for you.. haaha
+console.log("8: ", typeof iteratorOfFoodILike); //o/p 8:  object
+// Now this gives us an object, I think it will be cool to verify what this object contains.... and share with us...
+
+// According to the oracle definition "remove elements from the underlying collection during the iteration with well-defined semantics."
+console.log("9: ", iteratorOfFoodILike.next()); // o/p { value: 'Pounded Yam', done: false }
+// that returns an object to us... we can call  iteratorOfFoodILike.next().value on it to get the value
+// notice the done key as a flag false.... that is to show when it has finished iterating over the collection
+//If I want to use Iterator to loop through an object... I would use while true and break out of it when done is true.
+
+console.log("10: ", iteratorOfFoodILike.next().value); // o/p 10:  eba
+// I am calling value on it here....
